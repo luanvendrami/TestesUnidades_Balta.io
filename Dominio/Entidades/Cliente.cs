9aadd5a7934752c32f076bc2e4460313a5cb0310
luanvendrami <luan.vendrami@hotmail.com>
2022-01-03 21:13:03 -0300

    Add project files.

---
+++ Dominio/Entidades/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class Cliente
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public Cliente(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
