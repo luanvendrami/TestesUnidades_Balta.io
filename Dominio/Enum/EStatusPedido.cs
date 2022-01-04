46aad50f8608f69624cad55efc39f9e51002a5a9
luanvendrami <luan.vendrami@hotmail.com>
2022-01-03 21:37:26 -0300

    Criado enum para situações do pagamento

---
+++ Dominio/Enum/EStatusPedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Enum
+{
+    public enum EStatusPedido
+    {
+        AguadandoPagamento = 1,
+        AguardandoEntrega = 2,
+        Cancelado = 3
+    }
+}
